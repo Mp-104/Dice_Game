10cdcc6aaddde741ec45788fccced4061044141a
Maksim <Mp-104@hotmail.com>
2023-10-04 15:19:21 +0200

    added comments

--- src/com/Maksim/dice/Main.java
+++ src/com/Maksim/dice/Main.java
@@ -21,11 +21,11 @@ public class Main {
 
 
         System.out.println("Välkommen till tärning! Hur många spelar? Ange ett heltal");
-        while (!scanner.hasNextInt()) {                         // while loopen fortsätter köra scanner tills den får integer
+        while (!scanner.hasNextInt()) {                             // Checks for valid input, will loop until valid input is provided
             System.out.println("Det är inte ett heltal!");
             scanner.next();
         }
-        int players = scanner.nextInt();
+        int players = scanner.nextInt();                            // Variable used for amount of players, used in fori loop later
         System.out.println(players + " Spelare");
 
 
@@ -33,21 +33,21 @@ public class Main {
 
 
         System.out.println("Hur många rundor? Ange ett heltal");
-        while (!scanner.hasNextInt()) {
+        while (!scanner.hasNextInt()) {                              // Checks for valid input, will loop until valid input is provided
             System.out.println("Det är inte ett heltal!");
             scanner.next();
         }
-        int rounds = scanner.nextInt();
+        int rounds = scanner.nextInt();                              // Variable used for amount of rounds, used in fori loop later
         System.out.println(rounds + " Rundor");
 
         System.out.println(); // Radbrytning
 
         System.out.println("Hur många tärningar? Ange ett heltal!");
-        while (!scanner.hasNextInt()) {
+        while (!scanner.hasNextInt()) {                              // Checks for valid input, will loop until valid input is provided
             System.out.println("Det är inte ett heltal");
             scanner.next();
         }
-        int dices = scanner.nextInt();
+        int dices = scanner.nextInt();                               // Variable used for amount of dices, used later to in fori loop
         System.out.println(dices + " Tärningar");
 
         scanner.nextLine();   // player1.name = scanner.nextLine(); below was skipped, for some reason, this line prevents that and is skipped instead
@@ -56,24 +56,22 @@ public class Main {
         System.out.println(); // Radbrytning
 
 
-        for (int i = 0; i < players; i++) {
+        for (int i = 0; i < players; i++) {   // Loops for the amount of chosen players
 
             //create Unique players
             System.out.println("Ange namn för spelare " + (i+1 )); // +1 to better represent amount of players
-            Player player = new Player ();
-            player.name = scanner.nextLine();
+            Player player = new Player ();                        // Creates new player
+            player.name = scanner.nextLine();                     // User names new player
 
             System.out.println("Spelare " + (i+1) + ": " + player.name);
-            playerlist.add(player);
+            playerlist.add(player);                                         // Adds new player to ArrayList
             System.out.println(); // Radbrytning
 
         }
 
 
         System.out.println("Medverkande spelare: ");
-
-
-        for (int i = 0; i < playerlist.size(); i++) {
+        for (int i = 0; i < playerlist.size(); i++) {  // Lists participating players by name
 
             System.out.println(playerlist.get(i).name);
 
@@ -84,35 +82,35 @@ public class Main {
         scanner.nextLine();
         System.out.println(); // radbrytning
 
-        for (int i = 0; i < rounds; i++) {   // rounds är en variabel som spelare matar in med en int, loopen körs så många ggr
+
+        // Game logic
+        for (int i = 0; i < rounds; i++) {   // Loops for the amount of chosen rounds
             System.out.println("Runda " + (i+1) );
             System.out.println("Tryck enter för att fortsätta");
             scanner.nextLine();
 
-            for (int j = 0; j < playerlist.size(); j++) {
+            for (int j = 0; j < playerlist.size(); j++) {  // Loops for each player in the ArrayList
                 System.out.println(playerlist.get(j).name + "'s tur");
                 System.out.println("Tryck enter för att slå tärning");
                 scanner.nextLine();
 
 
-                for (int k = 0; k < dices; k++) {
-                    int dice = random.nextInt(1,7);
+                for (int k = 0; k < dices; k++) {                 // Loops for amount of chosen dices
+                    int dice = random.nextInt(1,7);  // Simulates dice throw
                     System.out.println("Tärning " + (k+1) + ": " + dice);
 
-                    playerlist.get(j).score += dice;
-
+                    playerlist.get(j).score += dice;              // Tallies the score
 
                 }
                 System.out.println(playerlist.get(j).name + " har " + playerlist.get(j).score + " poäng!") ;
                 System.out.println(); // Radbrytning
 
 
-
             }
-
-
-
         }
+
+
+
 
         System.out.println("Tryck enter för en sammanställning av resultatet");
         scanner.nextLine();
@@ -120,19 +118,21 @@ public class Main {
         System.out.println(); // Radbrytning
         System.out.println("Slutresultatet: ");
 
-        Collections.sort(playerlist , Collections.reverseOrder());
+
+        // Calculate winner
+        Collections.sort(playerlist , Collections.reverseOrder()); // sorts playerlist in reverse order based on int score, implements sorting interface in Player class
 
         for (int i = 0; i < playerlist.size(); i++) {
-            System.out.println(playerlist.get(i).score + " poäng: " + playerlist.get(i).name);
+            System.out.println(playerlist.get(i).score + " poäng: " + playerlist.get(i).name); // presents players in descending order
         }
-        if (playerlist.size() > 1) {
-            if (playerlist.get(0).score == playerlist.get(1).score) {
+        if (playerlist.size() > 1) {  // checks if more than one player present
+            if (playerlist.get(0).score == playerlist.get(1).score) {    // Compares top two players based on score
                 System.out.println(); // Radbrytning
                 System.out.println("Det blev oavgjort");
 
             } else {
                 System.out.println(); // Radbrytning
-                System.out.println(playerlist.get(0).name + " Vann!");
+                System.out.println(playerlist.get(0).name + " Vann!");      // Declares a winner if more than 1 player and not == score
 
             }
         }
